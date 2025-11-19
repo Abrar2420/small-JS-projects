ec5139e8e38a373b110d50afcf81e8e6c79b0318
Abrar242002 <abrar242002@gmail.com>
2025-11-19 23:11:11 +0600

    TODO list created

---
+++ todo list/todo.js
@@ -0,0 +1,37 @@
+const todoInput = document.querySelector(".add_todo");
+const addBtn = document.querySelector(".add_btn");
+const todoList = document.querySelector(".todo_list");
+
+const createTodoItem = () => {
+  let todoItem = document.createElement("li");
+  todoItem.classList.add("todo_list_item");
+
+  let todoCheck = document.createElement("input");
+  todoCheck.classList.add("todo_check");
+  todoCheck.setAttribute("type", "checkbox");
+
+  let todoText = document.createElement("span");
+  todoText.classList.add("todo_text");
+  todoText.textContent = todoInput.value;
+
+  let todoBtn = document.createElement("button");
+  todoBtn.classList.add("todo_btn");
+  todoBtn.textContent = "Delete";
+
+  todoCheck.addEventListener("click", () => {
+    todoText.classList.toggle("todo_done");
+  });
+
+  todoBtn.addEventListener("click", () => {
+    todoItem.remove();
+  });
+
+  todoItem.append(todoCheck, todoText, todoBtn);
+  todoList.appendChild(todoItem);
+};
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value !== "") {
+    createTodoItem();
+  }
+});
