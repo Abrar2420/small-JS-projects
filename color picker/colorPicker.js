0cde925c6f9e3732f0a93dbaca2772f217c214a3
Abrar242002 <abrar242002@gmail.com>
2025-11-19 19:11:10 +0600

    color picker, comment added

--- color picker/colorPicker.js
+++ color picker/colorPicker.js
@@ -17,6 +17,7 @@ sliders.forEach((slider) => {
   updateFill();
 });
 
+// getting the input and color box
 const color = document.querySelector(".color_box");
 const red = document.querySelector("#red");
 const green = document.querySelector("#green");
@@ -27,44 +28,50 @@ const wrapper = document.querySelector(".wrapper");
 
 // let b = document.querySelector(".#btn");
 
+// changes the style (background color)
+// to the value of the range dynamically
 const showColor = () => {
   let NewColor =
     (color.style.background = `rgb(${red.value}, ${green.value}, ${blue.value})`);
   return NewColor;
 };
+
+// adding the eventListner to all the range inputs
 red.addEventListener("input", showColor);
 green.addEventListener("input", showColor);
 blue.addEventListener("input", showColor);
-showColor();
+showColor(); // calling the function afterwards
 
+// creating, styling and adding the popup
 let popup = document.createElement("div");
 popup.classList.add("notify");
 popup.textContent = "Copied! to clip-board";
 wrapper.appendChild(popup);
 
 const showPopup = () => {
-  popup.classList.add("notify2");
+  popup.classList.add("notify2"); // adds the style for the popup position
   setTimeout(() => {
-    popup.classList.remove("notify2");
+    popup.classList.remove("notify2"); // removes the class after 1.5s
   }, 1500);
 };
 
 copy.addEventListener("click", () => {
-  navigator.clipboard.writeText(showColor());
+  navigator.clipboard.writeText(showColor()); // copies the provided text to clip-board
   showPopup();
 });
 
-console.log(showColor());
+// console.log(showColor());
 
+// getting the button
 const button = document.querySelector("#btn");
 
 const randomColor = () => {
-  let randomRed = Math.floor(Math.random() * 255);
-  let randomGreen = Math.floor(Math.random() * 255);
-  let randomBlue = Math.floor(Math.random() * 255);
+  let randomRed = Math.floor(Math.random() * 255); // random red value
+  let randomGreen = Math.floor(Math.random() * 255); // random blue value
+  let randomBlue = Math.floor(Math.random() * 255); // random green value
   color.style.background = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
 };
 
 button.addEventListener("click", () => {
-  randomColor();
+  randomColor(); //calling the function when clicked on button
 });
