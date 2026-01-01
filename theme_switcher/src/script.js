89b68a796341d024e86a97a3c03cee232e1e34ce
Abrar242002 <abrar242002@gmail.com>
2026-01-01 18:52:40 +0600

    JS added for theme switcher

--- theme_switcher/src/script.js
+++ theme_switcher/src/script.js
@@ -1,6 +1,28 @@
 // console.log("hello");
 
+const display = document.querySelector("#display");
+
+// console.log(display);
 const themeBtn = document.querySelector("#theme-btn");
-console.log(themeBtn);
 const themeTxt = document.querySelector("#theme-txt");
-console.log(themeTxt);
+// console.log(themeTxt);
+
+themeBtn.addEventListener("click", () => {
+  if (themeBtn.classList.contains("dark_btn")) {
+    display.classList.remove("light");
+    display.classList.add("dark");
+
+    themeBtn.classList.remove("dark_btn");
+    themeBtn.classList.add("light_btn");
+
+    themeTxt.textContent = "light mode";
+  } else if (themeBtn.classList.contains("light_btn")) {
+    display.classList.remove("dark");
+    display.classList.add("light");
+
+    themeBtn.classList.remove("light_btn");
+    themeBtn.classList.add("dark_btn");
+
+    themeTxt.textContent = "dark mode";
+  }
+});
