964dcafc52fe55b9c9cdabb0a57ac7bad9720952
Abrar242002 <abrar242002@gmail.com>
2026-01-01 19:01:15 +0600

    comments added on script.js

--- theme_switcher/src/script.js
+++ theme_switcher/src/script.js
@@ -1,21 +1,25 @@
-// console.log("hello");
-
+// storing the main tag
 const display = document.querySelector("#display");
 
-// console.log(display);
+// storing the theme switcher btn and bth text
 const themeBtn = document.querySelector("#theme-btn");
 const themeTxt = document.querySelector("#theme-txt");
-// console.log(themeTxt);
 
+// getting when theme switcher is clcked
 themeBtn.addEventListener("click", () => {
+  // this block of code runs every time the btn is clicked
+
+  // if the btn has the class of "dark_btn" this if block runs
   if (themeBtn.classList.contains("dark_btn")) {
-    display.classList.remove("light");
-    display.classList.add("dark");
+    display.classList.remove("light"); // removing the light class form the main
+    display.classList.add("dark"); // adding the dark class instead, the dark theme applies here
 
-    themeBtn.classList.remove("dark_btn");
-    themeBtn.classList.add("light_btn");
+    themeBtn.classList.remove("dark_btn"); // removing the dark_btn class
+    themeBtn.classList.add("light_btn"); // adding the light_btn class
 
-    themeTxt.textContent = "light mode";
+    themeTxt.textContent = "light mode"; // changing the text
+
+    // if esle dose the opposite of the if block
   } else if (themeBtn.classList.contains("light_btn")) {
     display.classList.remove("dark");
     display.classList.add("light");
