b6f53d56e5a81f84b24034a2d2ba12b1fb031816
Abrar242002 <abrar242002@gmail.com>
2026-01-01 18:27:13 +0600

    script.js added, theme switcher accessed

---
+++ theme_switcher/src/script.js
@@ -0,0 +1,6 @@
+// console.log("hello");
+
+const themeBtn = document.querySelector("#theme-btn");
+console.log(themeBtn);
+const themeTxt = document.querySelector("#theme-txt");
+console.log(themeTxt);
